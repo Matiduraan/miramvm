65c12b5b98b6b14b62157e00255abc1aa23fca03
matiduraan <matiduraan@gmail.com>
2021-06-18 01:23:44 -0300

    Intento de segmentacion

---
+++ TpMiRamMemoria/so-commons-library-master/cmake-build-debug/CMakeFiles/MiRamMemoria_mio_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiRamMemoria_mio_.dir/main.c.obj"
+  "MiRamMemoria_mio_.exe"
+  "MiRamMemoria_mio_.exe.manifest"
+  "MiRamMemoria_mio_.pdb"
+  "libMiRamMemoria_mio_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MiRamMemoria_mio_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- TpMiRamMemoria/so-commons-library-master/src/memoria.c
+++ TpMiRamMemoria/so-commons-library-master/src/memoria.c
@@ -1,4 +1,4 @@
-#include "commons/collections/list.c"
+#include "commons/collection/list.c"
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -11,19 +11,17 @@ typedef struct
 } segmentoEnTabla_t;
 typedef struct
 {
-    int *ptrHuecoLibre;
+    int ptrHuecoLibre;
     int tamanio;
 } espacio_t;
 t_list *listaSegmentos;
-//void *guardar_en_memoria(void *);
+void *guardar_en_memoria(segmentoEnTabla_t);
+void *memoria;
 int main()
 {
-    void *memoria;
+
     memoria = malloc(1024);
     printf("puntero inicial memoria: %d \n", memoria);
-
-    int a = 500;
-    char b = 'b';
     segmentoEnTabla_t unSegmento;
     unSegmento.inicio = memoria;
     unSegmento.nroSegmento = 0;
@@ -32,6 +30,9 @@ int main()
     otroSegmento.inicio = (memoria + sizeof(unSegmento));
     otroSegmento.nroSegmento = 1;
     otroSegmento.tamanio = 16;
+    printf("Llegue hasta aca\n");
+    guardar_en_memoria(unSegmento);
+    /*
     *(segmentoEnTabla_t *)memoria = unSegmento;
     *(segmentoEnTabla_t *)(memoria + sizeof(unSegmento)) = otroSegmento;
     listaSegmentos = list_create();
@@ -40,25 +41,33 @@ int main()
     printf("Direccion inicial del segmento: %d, Numero de segmento: %d, Tamaño de segmento: %d  \n", *(segmentoEnTabla_t *)memoria, *(segmentoEnTabla_t *)(memoria + sizeof(int)), *(segmentoEnTabla_t *)(memoria + 2 * sizeof(int)));
     printf("Direccion inicial del segmento: %d, Numero de segmento: %d, Tamaño de segmento: %d  \n", *(segmentoEnTabla_t *)(memoria + sizeof(unSegmento)), *(segmentoEnTabla_t *)(memoria + sizeof(unSegmento) + sizeof(int)), *(segmentoEnTabla_t *)(memoria + 2 * sizeof(unSegmento)));
     printf("Primer elemento de la lista de segmentos : %d \n", list_get(listaSegmentos, 0));
+    */
     free(memoria);
     return 0;
 }
-/*void *guardar_en_memoria(void *payload)
+//Cambie esto porque me fue imposible hacer funcionar el pasar un void* como parametro asi que me parecio lo mejor
+//Ponerle 3 parametros, que sean pcb, tcb y archivoDeTareas y que le pases NULL en los que no te interesan
+//Y mandar solo lo que necesitas
+void *guardar_en_memoria(segmentoEnTabla_t tcb)
 {
+    printf("Llegue hasta aca2\n");
     int segmentoAsignado;
     bool estaLibre;
+    int huecoLibre;
     int w = 0;
     espacio_t *espaciosLibres;
+    printf("Llegue hasta aca3\n");
+
     if (list_is_empty(listaSegmentos))
     {
         printf("Se entro al if");
 
         //Guarda directamente en memoria y lo agrega a la lista de segmentos
-        *memoria = *payload;
+        *(segmentoEnTabla_t *)memoria = tcb;
         segmentoEnTabla_t *nuevoSegmento;
         nuevoSegmento->inicio = memoria;
         nuevoSegmento->nroSegmento = 0;
-        nuevoSegmento->tamanio = 2 * sizeof(int);
+        nuevoSegmento->tamanio = sizeof(tcb);
         list_add(listaSegmentos, nuevoSegmento);
         segmentoAsignado = nuevoSegmento->nroSegmento;
     }
@@ -67,25 +76,25 @@ int main()
         printf("Lista de segmentos pito");
         for (int i = 0; i <= list_size(listaSegmentos); i++)
         {
-            int tamActual = list_get(listaSegmentos, i)->tamanio;
-            int inicioActual = list_get(listaSegmentos, i)->inicio;
-            int inicioSig = list_get(listaSegmentos, i + 1)->inicio;
-            if (inicioSig != NULL)
+            int tamActual = *(int *)(list_get(listaSegmentos, i) + 2 * sizeof(int));
+            int inicioActual = *(int *)list_get(listaSegmentos, i);
+            int inicioSig = *(int *)list_get(listaSegmentos, i + 1);
+            if ((void *)inicioSig != NULL)
             {
-                int huecoLibre = inicioSig - (inicioActual + tamActual);
+                huecoLibre = inicioSig - (inicioActual + tamActual);
             }
             else
             {
-                int huecoLibre = memoria + (256) - (inicioActual + tamActual);
+                huecoLibre = *(int *)memoria + ((1024) - (inicioActual + tamActual));
             }
             if (huecoLibre != 0)
             {
                 //best fit
-                espaciosLibres[w]->ptrHuecoLibre = inicioActual + tamActual;
+                espaciosLibres[w].ptrHuecoLibre = inicioActual + tamActual;
                 w++;
 
                 //First fit
             }
         }
     }
-}*/
+}
